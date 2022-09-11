c24543b2b5b4ed7450407d05990ef6e6addc978c
kwa-dev <kwabohuncho@gmail.com>
2022-09-11 00:46:13 -0700

    use of puts function with betty edit format

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
- *main - this C program prints "Programming is like building a multilingual puzzle", followed by a new line.
- *
+ *main - this C program prints "Programming is like building a multilingual
+ *puzzle", followed by a new line.
+	*Return: 0 if exited properly
  */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
