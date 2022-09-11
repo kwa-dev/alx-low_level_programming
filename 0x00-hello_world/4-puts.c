c7000ace80d104b314ea5cd8c8a2b66758633a07
kwa-dev <kwabohuncho@gmail.com>
2022-09-11 00:06:31 -0700

    use of puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ *main - this C program prints "Programming is like building a multilingual puzzle", followed by a new line.
+ *
+ */
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return(0);
+}
