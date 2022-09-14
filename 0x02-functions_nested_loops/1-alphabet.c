0fce2635310df4c5b5943505a543b4f23ae7db46
kwa-dev <kwabohuncho@gmail.com>
2022-09-14 11:57:43 -0700

    a function that prints the alphabet, in lowercase using header file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		_putchar(alph);
+	}
+	_putchar('\n');
+}
