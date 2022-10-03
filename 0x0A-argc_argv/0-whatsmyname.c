8e67124ad8cc9d22eff8deb3761c51ac01b7bb58
kwa-dev <kwabohuncho@gmail.com>
2022-10-02 22:05:53 -0700

    printing file name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints file name
+ *@argc: argument counter
+ *@argv: argument vector
+ *
+ *Return: 0 for success else failure
+ */
+int main(int __attribute__ ((unused)) argc, char *argv[])
+{
+	printf(" %s \n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+this file contains argc and argv which are arguments to the main function 
